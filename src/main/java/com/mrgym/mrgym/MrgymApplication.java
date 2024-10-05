f335843de973e974448665421f4433df2e0c2a7f
AtunN25 <franklin25.2001@gmail.com>
2024-10-04 22:08:54 -0500

    se habilito los cors para otros dominos o para usarlo localmente con otros frameworks

--- src/main/java/com/mrgym/mrgym/MrgymApplication.java
+++ src/main/java/com/mrgym/mrgym/MrgymApplication.java
@@ -2,10 +2,11 @@ package com.mrgym.mrgym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;*/
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MrgymApplication {
@@ -14,18 +15,18 @@ public class MrgymApplication {
 		SpringApplication.run(MrgymApplication.class, args);
 	}
 
-	//@Configuration
-	/*public static class Myconfiguration{
+	@Configuration
+	public static class Myconfiguration{
 		@Bean
 		public WebMvcConfigurer corsConfigurer(){
 			return new WebMvcConfigurer() {
 				@Override
-				public void addCorsMappings(CorsRegistry registry) {
+				public void addCorsMappings(@NonNull CorsRegistry registry) {
 					registry.addMapping("/**")
 							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 				}
 			};
 		}
-	}*/
+	}
 
 }
